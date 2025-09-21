0def728d27963d5c4279987b7f7c220e0f55ac08
R4DHW3N <rad_boul@hotmail.com>
2025-09-21 13:29:02 +0000

    test

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// ici test unitaire de projet
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+    <title>Un document HTML</title>
+
+</head>
+
+<body>
+
+    <p>Hello World!<p>
+
+</body>
+
+</html>
